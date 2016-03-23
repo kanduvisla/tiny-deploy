7c85fb0de8595bc51ae0321863576fb1f59ddb18
Giel Berkers <info@gielberkers.com>
2016-03-23 22:16:03 +0100

    Basic setup of tiny deploy

--- td.sh
+++ td.sh
@@ -21,6 +21,10 @@ done
 # Check if all required parameters are set:
 if [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$SSH_SOURCE" ] || [ -z "$SSH_DESTINATION" ]; then
     echo 'Tiny Deploy
+---
+A small deployment script that deploys a source directory to a target
+directory using SSH. The only requirement is that the remote server
+accepts the user by authenticating with a public key.
 
 Usage:
     ./td.sh -husd
@@ -37,3 +41,9 @@ Example:
 
     exit 1;
 fi
+
+# Create remote directory if it doesn't exist:
+ssh "$SSH_USER"@"$SSH_HOST" mkdir -p "$SSH_DESTINATION"
+
+# Attempt deployment with rsync:
+rsync -avz -e ssh "$SSH_SOURCE"/* "$SSH_USER"@"$SSH_HOST":"$SSH_DESTINATION"
