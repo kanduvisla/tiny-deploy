0739b690a82a6a7bd838bff32325a671b959661b
Giel Berkers <giel@happy-online.nl>
2016-03-24 09:55:30 +0100

    Updated tiny deploy

--- readme.md
+++ readme.md
@@ -10,6 +10,13 @@ _A small utility to deploy to a remote server_
     
 This example copies all files from `./test/*` to `example@server.org` in the folder `/home/example/public/`.
 
+## Prerequisites
+
+This script requires the program `rsync`to be installed on your system. Furthermore you need to know that the
+authentication with the remote server is done with SSH keys. This means that your public key needs to be known
+by the remote user@server that you are trying to deploy to. If your not familiar on how to do this, there are
+various articles on the Internet that explain how to do this. [This one for example](https://kb.iu.edu/d/aews).
+
 ## Pre- and post-deployment tasks
 
 To perform tasks before or after the deployment, add a shell script called `td_before.sh` and `td_after.sh` 
@@ -20,3 +27,24 @@ of your deployment.
     # td_after.sh
     echo "Deployment finished in $TD_ROOT"
 
+## Ignored files and folders
+
+One thing you might want to control is a list if files that should not be deployed, or files on the server that
+should not be removed. To ignore files locally or remote, add a file called `td_ignore.txt`  to the root of your 
+source folder, in which you can include files and/or folders that should be ignored during deployment. 
+
+    do_not_deploy.txt
+    readme.txt
+    exclude_dir/
+    uploads/
+    cache/
+    
+_Please note that due to rsyncs' nature, when a file or directory is added later on to `td_ignore.txt`, it's not
+automatically deleted from the server as well. For this reason, when editing `td_ignore.txt`, some additional 
+deleting on the remote server might be required._
+
+## Disclaimer
+
+Please note that the usage of this script is completely at your own risk and the author cannot be held 
+responsible if anything breaks. If you encounter an issue, please report it or better yet: fix it and send a
+pull request.
--- td.sh
+++ td.sh
@@ -20,23 +20,33 @@ done
 
 # Check if all required parameters are set:
 if [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$SSH_SOURCE" ] || [ -z "$SSH_DESTINATION" ]; then
-    echo 'Tiny Deploy
+    printf '\033[1;37mTiny Deploy\033[0m
 ---
 A small deployment script that deploys a source directory to a target
 directory using SSH. The only requirement is that the remote server
 accepts the user by authenticating with a public key.
 
-Usage:
+\033[1;37mUsage:\033[0m
     ./td.sh -husd
 
-Parameters:
+\033[1;37mParameters:\033[0m
     -h      The SSH Host to deploy to
     -u      The username to use
     -s      The source directory to deploy from
     -d      The target directory to deploy to
 
-Example:
+\033[1;37mExample:\033[0m
     ./td.sh -h server.org -u example -s ./test -d /home/example/public
+    
+To perform actions prior before deployment on the remote server, add a file
+called td_before.sh to the root of your source folder. Likewise, to perform
+actions after a deployment add a file called td_after.sh
+
+To ignore files from deployment (locally and/or remote), add a file called
+td_ignore.txt to the root of your source folder containing files and folders
+that should be ignored.
+
+For more information, see \033[0;36mhttps://github.com/kanduvisla/tiny-deploy\033[0m
 ';
 
     exit 1;
@@ -44,14 +54,21 @@ fi
 
 # Perform remote actions before deployment:
 if [ -e "$SSH_SOURCE"/td_before.sh ]; then
-    ssh "$SSH_USER"@"$SSH_HOST" TD_ROOT="$SSH_DESTINATION" "$SSH_DESTINATION"/td_before.sh
+    ssh "$SSH_USER"@"$SSH_HOST" TD_ROOT="$SSH_DESTINATION" [ -e "$SSH_DESTINATION"/td_before.sh ] && "$SSH_DESTINATION"/td_before.sh 
 fi;
 
 # Create remote directory if it doesn't exist:
 ssh "$SSH_USER"@"$SSH_HOST" mkdir -p "$SSH_DESTINATION"
 
 # Attempt deployment with rsync:
-rsync -avz -e ssh "$SSH_SOURCE"/* "$SSH_USER"@"$SSH_HOST":"$SSH_DESTINATION"
+if [ -e "$SSH_SOURCE"/td_ignore.txt ]; then
+    # Ignore local files
+    rsync -avz --del --exclude-from "$SSH_SOURCE/td_ignore.txt" -e ssh "$SSH_SOURCE"/ "$SSH_USER"@"$SSH_HOST":"$SSH_DESTINATION"
+    ssh "$SSH_USER"@"$SSH_HOST" rm "$SSH_DESTINATION"/td_ignore.txt
+else
+    # Default deployment
+    rsync -avz --del -e ssh "$SSH_SOURCE"/ "$SSH_USER"@"$SSH_HOST":"$SSH_DESTINATION"
+fi
 
 # Perform remote actions after deployment:
 if [ -e "$SSH_SOURCE"/td_after.sh ]; then
